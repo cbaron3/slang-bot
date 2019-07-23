2edaaad3ea2b9500736079c46159531cd9d9d0ec
Carl Baron <carbaro196@gmail.com>
2019-07-23 14:05:02 -0700

    Started adding code to scrape urban dictionary definitions

--- app.py
+++ app.py
@@ -3,6 +3,28 @@ import os
 from secrets import *
 import praw
 
+import urllib, json
+
+_URBANDICT_URL = "http://api.urbandictionary.com/v0/define?term="
+
+def _run_urbandict(search):
+    safe_search = ""
+    words = ' '.join(search.split()).split(' ')
+    for idx, word in enumerate(words):
+        if len(words) == idx+1:
+            safe_search += "%s" % urllib.parse.quote_plus(word)
+        else:
+            safe_search += "%s+" % urllib.parse.quote_plus(word)
+    with urllib.request.urlopen("http://www.python.org") as url:
+        print(url.read())
+        # Need to parse html for div meaning and example
+        # may need to parse all responses to find most upvoted
+        # filters: top-response, most-upvotes, best-vote-ratio
+        #return json.loads(url.read())
+
+def urbandict(word):
+    return _run_urbandict(word)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -19,6 +41,11 @@ def p():
     sub = reddit.subreddit('all')
     print(sub.top())
     return "PRAW Testing endpoint"
+
+@app.route('/urban')
+def u():
+    print( urbandict('netflix') )
+    return "Urban Dict Testing endpoint"
 
 if __name__ == '__main__':
     app.run()
