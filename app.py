0d379196c2cb7d683862213ec2b16d429c18b21c
Carl Baron <carbaro196@gmail.com>
2019-07-23 13:36:56 -0700

    Added praw (reddit api) to project and verified access

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask
+import os
+from secrets import *
+import praw
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     """Return homepage."""
     return "Hello, World!"
+
+@app.route('/praw')
+def p():
+    reddit = praw.Reddit( user_agent=reddit_user_agent, client_id=reddit_client_id, client_secret=reddit_secret,
+                            username=reddit_username, password=reddit_password )
+    print( reddit.read_only )
+    # print( reddit.user.me() )
+    sub = reddit.subreddit('all')
+    print(sub.top())
+    return "PRAW Testing endpoint"
 
 if __name__ == '__main__':
     app.run()
