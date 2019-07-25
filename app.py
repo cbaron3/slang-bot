416adae4135c4ea8b1177e956f465dca850ffc6e
Carl Baron <carbaro196@gmail.com>
2019-07-25 08:18:40 -0700

    Simple praw endpoint is working now, bot can be invoked and responds in testingground4bots subreddit

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import os
 from secrets import *
 import praw
 from flask import request, flash
+from multiprocessing import Process, Value
+import time
+import pytz
+import datetime
 
 from urbandict import define
 
@@ -10,7 +14,8 @@ app = Flask(__name__)
 
 reddit = praw.Reddit( user_agent=reddit_user_agent, client_id=reddit_client_id, client_secret=reddit_secret,
                             username=reddit_username, password=reddit_password )
-print( reddit.read_only )
+
+START_TIME = datetime.datetime.now(tz=pytz.utc).timestamp() 
 
 @app.route('/')
 def index():
@@ -31,7 +36,7 @@ def p():
     top_posts = []
 
     for t in top:
-        top_posts.append(t.selftext)
+        top_posts.append( t.selftext )
 
     return " ".join( top_posts )
 
@@ -47,5 +52,35 @@ def u():
     print( title, meaning, example)
     return title + " " + meaning + " " + example
 
+subreddit = reddit.subreddit('testingground4bots')
+keyphrase = '!slang-bot'
+
+def bot_invoke( ):
+    while True:
+        print('bot backend running')
+        for comment in subreddit.stream.comments():
+            if keyphrase in comment.body:
+                # If keyphrase is in body, remove keyphrase from body and use rest of text as term
+                # TODO: GEt user who submitted
+                # Track already visited bot requests. Could use timestamp
+                print(comment.body)
+                print( START_TIME, comment.created_utc)
+                term = comment.body.replace( keyphrase, '' )
+                term.strip()
+
+                if term != '' and comment.created_utc > START_TIME:
+                    try:
+                        title, meaning, example = define(term) 
+                        print( title, meaning, example)
+                        comment.reply( "Slang Bot defines " + title + " as : \n" + meaning )
+                    except:
+                        print('To frequent')
+        print('bot backend running')
+        time.sleep(1)
+
 if __name__ == '__main__':
-    app.run()
+    recording_on = Value('b', True)
+    p = Process(target=bot_invoke, args=())
+    p.start()  
+    app.run(debug=True, use_reloader=False)
+    p.join()
